4fdd00c67d82d664620fab84b8f3a62b3eb1f0e3
r99praveen <r99praveen@gmail.com>
2021-09-08 14:17:09 +0530

    first commit

---
+++ firstApp/src/main/java/firstApp/HelloWorld.java
@@ -0,0 +1,11 @@
+package firstApp;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Praveen! Welcome to matrix world....  ");
+
+	}
+
+}
